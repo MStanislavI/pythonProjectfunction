d26e423b36e214d6bcafbee956997a2d7ace817d
stanislav matytsyn <matytsyn1983@gmail.com>
2024-10-08 23:00:33 +0300

    fixes

--- src/masks.py
+++ src/masks.py
@@ -1,14 +1,21 @@
-def get_mask_card_number(card_number: str) -> str:
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[int, str]) -> Union[int, str]:
     """Функция, которая маскирует номер карты"""
 
-    return f"{card_number[:4]}{card_number[4:6]}** **** {card_number[12:]}"
+    return f"{str(card_number)[:4]}{str(card_number)[4:6]}** **** {str(card_number)[12:]}"
 
 
-def get_mask_account(account_number: str) -> str:
+if __name__ == "__main__":
+    print(get_mask_card_number)
+
+
+def get_mask_account(count_number: Union[int, str]) -> Union[int, str]:
     """Функция, которая маскирует номер счета"""
 
-    return f"**{account_number[-4:]}"
+    return f"**{str(count_number)[-4:]}"
 
 
-print(get_mask_account("73654108430135874305"))
-print(get_mask_card_number("7000792289606361"))
+if __name__ == "__main__":
+    print(get_mask_account)
