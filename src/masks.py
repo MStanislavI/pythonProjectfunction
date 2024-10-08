a4030a60a855ab6dcb62eb12fdd368fb60544033
stanislav matytsyn <matytsyn1983@gmail.com>
2024-10-08 20:50:42 +0300

    src/masks.py

---
+++ src/masks.py
@@ -0,0 +1,14 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция, которая маскирует номер карты"""
+
+    return f"{card_number[:4]}{card_number[4:6]}** **** {card_number[12:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция, которая маскирует номер счета"""
+
+    return f"**{account_number[-4:]}"
+
+
+print(get_mask_account("73654108430135874305"))
+print(get_mask_card_number("7000792289606361"))
